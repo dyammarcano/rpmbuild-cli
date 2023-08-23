396bad5a2741d406dbe2a5f2542772329ad0decb
Dyam Marcano <dyam.marcano@gmail.com>
2023-08-23 00:43:30 -0300

    Remove hard-coded credentials, refactor for dynamic config file creation
    In order to improve security and flexibility, hard-coded credentials and configurations in 'repodata.toml' have been removed, and functionality for dynamic creation of an initial configuration file has been implemented. This change includes the introduction of a configuration struct in 'structures.go' that facilitates improved handling of variables such as host, user, pass, and database info. The 'init.go' and 'build.go' files have also been updated to utilise the new method for handling configurations. Error handling in the 'CriateFoldersStructure' function has also been enhanced to now return and handle errors as opposed to using panic.

--- cmd/check.go
+++ cmd/check.go
@@ -16,21 +16,17 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("check called")
-	},
+	RunE: CheckFunc,
 }
 
 func init() {
 	rootCmd.AddCommand(checkCmd)
+}
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// checkCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// checkCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func CheckFunc(cmd *cobra.Command, args []string) error {
+	fmt.Println("check called")
+	//TODO check the database for the source and spec file
+	//TODO check the database for the version
+	//TODO check the database for the build folder
+	return nil
 }
--- cmd/clean.go
+++ cmd/clean.go
@@ -16,21 +16,15 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("clean called")
-	},
+	RunE: CleanFunc,
 }
 
 func init() {
 	rootCmd.AddCommand(cleanCmd)
+}
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// cleanCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// cleanCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func CleanFunc(cmd *cobra.Command, args []string) error {
+	fmt.Println("clean called")
+	//TODO delete the build folder
+	return nil
 }
